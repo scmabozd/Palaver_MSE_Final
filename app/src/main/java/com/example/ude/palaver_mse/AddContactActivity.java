14b2cd61f0ad48e5f9955b47856281034915fdb4
scmabozd <bozdev_m@yahoo.de>
2017-06-07 17:09:06 +0200

    31.05.2017

--- app/src/main/java/com/example/ude/palaver_mse/AddContactActivity.java
+++ app/src/main/java/com/example/ude/palaver_mse/AddContactActivity.java
@@ -3,7 +3,7 @@ package com.example.ude.palaver_mse;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class addContact extends AppCompatActivity {
+public class AddContactActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
