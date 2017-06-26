9173403d7169a55c98f78717bbdc1345e34829c4
scmabozd <bozdev_m@yahoo.de>
2017-06-26 15:51:12 +0200

    31.05.2017

--- app/src/main/java/com/example/ude/palaver_mse/AppController.java
+++ app/src/main/java/com/example/ude/palaver_mse/AppController.java
@@ -3,6 +3,7 @@ package com.example.ude.palaver_mse;
  * Created by mbozd on 26.06.2017.
  */
 import android.app.Application;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.text.TextUtils;
 import com.android.volley.Request;
@@ -10,7 +11,6 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
 public class AppController extends Application {
-    private SharedPreferences prefs;
 
     public static final String TAG = AppController.class.getSimpleName();
 
@@ -51,12 +51,50 @@ public class AppController extends Application {
             mRequestQueue.cancelAll(tag);
         }
     }
+
+
+
+
+
+    //new
+
+    private SharedPreferences prefs;
+    public int notificationId = 1;
+
+    public void setContext(Context context) {
+        this.context = context;
+        prefs = context.getSharedPreferences("palaver", MODE_PRIVATE);
+    }
+
+    private Context context;
+
+    public SharedPreferences getPrefs() {
+        return prefs;
+    }
+
+    public void setPrefs(SharedPreferences prefs) {
+        this.prefs = prefs;
+    }
+
     public String getUsername() {
         return prefs.getString("username", null);
-
     }
+
+    public void setUsername(String username) {
+        SharedPreferences.Editor e = prefs.edit();
+        e.putString("username", username);
+        e.commit();
+    }
+
     public String getPassword() {
         return prefs.getString("password", null);
-
     }
+
+    public void setPassword(String password) {
+        SharedPreferences.Editor e = prefs.edit();
+        e.putString("password", password);
+        e.commit();
+    }
+
+
 }
