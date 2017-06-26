a0a22d2be79bab45dc5e065feca6d4badfd119ea
scmabozd <bozdev_m@yahoo.de>
2017-06-26 11:35:54 +0200

    31.05.2017

--- app/src/main/java/com/example/ude/palaver_mse/AppController.java
+++ app/src/main/java/com/example/ude/palaver_mse/AppController.java
@@ -15,9 +15,6 @@ import com.android.volley.toolbox.Volley;
 
 public class AppController extends Application {
 
-
-
-
     public static final String TAG = AppController.class.getSimpleName();
 
     private RequestQueue mRequestQueue;
