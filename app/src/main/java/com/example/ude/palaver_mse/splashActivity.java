34162c0553d2ba6be5d2294a23b83a2f80312c21
scmabozd <bozdev_m@yahoo.de>
2017-07-01 22:07:42 +0200

    31.05.2017

--- app/src/main/java/com/example/ude/palaver_mse/splashActivity.java
+++ app/src/main/java/com/example/ude/palaver_mse/splashActivity.java
@@ -2,6 +2,7 @@ package com.example.ude.palaver_mse;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -16,14 +17,17 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import java.util.ResourceBundle;
+
 public class splashActivity extends Activity {
-    String eingeloggt;
+    String eingeloggt = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        View decorView = getWindow().getDecorView();
+
+        final View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
         decorView.setBackgroundColor(Color.parseColor("#FF7EBF30"));
@@ -33,19 +37,46 @@ public class splashActivity extends Activity {
         final Animation an = AnimationUtils.loadAnimation(this, R.anim.rotate);
 
         v.startAnimation(an);
-        final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(splashActivity.this);
+
+        Context c = this;
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
         SharedPreferences.Editor editor = sp.edit();
+        boolean initial = Boolean.parseBoolean(null);
+
+        try {
+            eingeloggt = sp.getString("Eingeloggt",null);
+            if (eingeloggt.equals("ja") || (eingeloggt.equals("ja")))
+                initial = false;
+            Log.d("initial in TRY/catch", String.valueOf(initial));
+
+
+        }catch ( Exception e){
+            initial = true;
+            Log.d("initial in try/CATCH", String.valueOf(initial));
+        }
+        if (initial){
+            editor.putString("Eingeloggt", "nein");
+            editor.apply();
+            Log.d("Eingeloggt initial=t", sp.getString("Eingeloggt",null));
+
+        }else if (!initial){
+            editor.putString("Eingeloggt",  sp.getString("Eingeloggt",null));
+            editor.apply();
+            Log.d("Eingeloggt initial=f", sp.getString("Eingeloggt",null));
+
+        }
+
+        Log.d("Eingeloggt out IfElse", sp.getString("Eingeloggt",null));
+        eingeloggt = sp.getString("Eingeloggt",null);
         an.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
-                eingeloggt = sp.getString("Eingeloggt",null);
-                Log.d("Eingeloggt",eingeloggt);
 
             }
-
             @Override
             public void onAnimationEnd(Animation animation) {
                 finish();
+                Log.d("Eingeloggt", eingeloggt);
                 if (eingeloggt.equals("ja"))
                 {
                     Intent i = new Intent(getBaseContext(),contacts.class);
@@ -55,8 +86,8 @@ public class splashActivity extends Activity {
                     Intent i = new Intent(getBaseContext(),LoginActivity.class);
                     startActivity(i);
                 }
-            }
 
+            }
             @Override
             public void onAnimationRepeat(Animation animation) {
 
